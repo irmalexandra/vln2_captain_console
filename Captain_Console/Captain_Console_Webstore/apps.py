bf6420d283ac67b2b7725c9b3cb24a0679baf1f4
Loki Hopkins <nyms.hopkins@gmail.com>
2020-04-29 10:08:53 +0000

    Created project

---
+++ Captain_Console/Captain_Console_Webstore/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CaptainConsoleWebstoreConfig(AppConfig):
+    name = 'Captain_Console_Webstore'
---
+++ Captain_Console/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!F:\lokialex\git_repositories\VLN2_43\Captain_Console\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
