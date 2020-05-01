68fb735a7ccda05c2ee8d4f350ef4c96aeb1b53c
Loki Hopkins <nyms.hopkins@gmail.com>
2020-05-01 13:13:42 +0000

    new version of project

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!F:\lokialex\git_repositories\Captain_Console_43\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
