a37caea4d8318c5b6a347f244810347df5cc0fe7
R1klus <rikhardur.fridgeirsson@gmail.com>
2020-05-01 10:47:06 +0000

    remade project again

---
+++ captain_console/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\rikha\Documents\RU\Verklegt_2\VLN2_43\captain_console\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
