91cca821506dbf1b1922f96d45ed0d993af601b5
R1klus <rikhardur.fridgeirsson@gmail.com>
2020-05-01 17:31:27 +0000

    added all urls and paths to connect main apps to main

--- games/urls.py
+++ games/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     # localhost:8000/games/  <--- Path so far
-    path('', views.index, name="main-index"),
+    path('', views.index, name="games-index"),
 
 ]
