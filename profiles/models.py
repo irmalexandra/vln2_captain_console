48d9014f3d5f180ff19e8ceca200d544396fbadb
R1klus <rikhardur.fridgeirsson@gmail.com>
2020-05-02 13:15:29 +0000

    remade DB 7 times

--- profiles/models.py
+++ profiles/models.py
@@ -1,52 +1,40 @@
 from django.db import models
 
 # Create your models here.
-from carts.models import Orders
-from games.models import Games
+from carts.models import Order, ShippingInformation, PaymentInformation
+from games.models import Game
+from django.contrib.auth.models import User
 
 
-
-
-
-
-
-class Profiles(models.Model):
-    user = models.CharField(max_length=255)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     address_1 = models.CharField(max_length=255)
     address_2 = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     postcode = models.IntegerField()
     country = models.CharField(max_length=255)
     profile_image = models.CharField(max_length=999)
+    payment_information_id = models.ForeignKey(ShippingInformation, on_delete=models.CASCADE)
+    shipping_information_id = models.ForeignKey(PaymentInformation, on_delete=models.CASCADE)
 
 
-
-class Reviews(models.Model):
-    profileID = models.ForeignKey(Profiles, on_delete=models.CASCADE)
+class Review(models.Model):
+    profileID = models.ForeignKey(Profile, on_delete=models.CASCADE)
     rating = models.IntegerField()
     feedback = models.CharField(max_length=999)
     datetime = models.DateTimeField()
 
 
-
-
-class GameReviews(models.Model):
-    gameID = models.ForeignKey(Games, on_delete=models.CASCADE)
-    reviewID = models.ForeignKey(Reviews, on_delete=models.CASCADE)
-
+class GameReview(models.Model):
+    gameID = models.ForeignKey(Game, on_delete=models.CASCADE)
+    reviewID = models.ForeignKey(Review, on_delete=models.CASCADE)
 
 
 class OrderHistory(models.Model):
-    profileID = models.ForeignKey(Profiles, on_delete=models.CASCADE)
-    orderId = models.ForeignKey(Orders, on_delete=models.CASCADE)
-
-
-class Searches(models.Model):
-    search = models.CharField(max_length=255)
+    profileID = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    orderId = models.ForeignKey(Order, on_delete=models.CASCADE)
 
 
 class SearchHistory(models.Model):
-    searchID = models.ForeignKey(Searches, on_delete=models.CASCADE)
-    profileID = models.ForeignKey(Profiles, on_delete=models.CASCADE)
-
-
+    search = models.TextField()
+    profileID = models.ForeignKey(Profile, on_delete=models.CASCADE)
