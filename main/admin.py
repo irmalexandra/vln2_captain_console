bdd39061c23451a572bbe1d16e3e5e3435aaa54f
The69Kang <Emmsje legend>
2020-05-02 19:57:43 +0000

    DB finito(?)

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from main.models import Manufacturer
+from main.models import Manufacturer, ProductImages
 
 admin.site.register(Manufacturer)
+admin.site.register(ProductImages)
