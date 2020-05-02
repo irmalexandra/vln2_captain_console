0a5e154f2784a0a25ac2532ea7863a844089ac5c
The69Kang <Emmsje legend>
2020-05-02 14:39:06 +0000

    Admin access to make everything

--- consoles/admin.py
+++ consoles/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from consoles.models import Console
-from main.models import Manufacturer
+
 
 admin.site.register(Console)
-admin.site.register(Manufacturer)
--- games/models.py
+++ games/models.py
@@ -6,8 +6,12 @@ from main.models import Product
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
 
 
 class Game(Product):
     console_id = models.ForeignKey(Console, on_delete=models.SET_NULL, null=True)
-    genre_id = models.ForeignKey(Genre, on_delete=models.CASCADE, null=True)
+    genre_id = models.ForeignKey(Genre, on_delete=models.CASCADE, null=True) # TODO: Rethink this one, chief
+
+
