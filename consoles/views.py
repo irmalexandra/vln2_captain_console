68e16116ec2cb20c24bf8476c668dc26b0f712c6
mg <margret@margud.com>
2020-05-03 16:20:30 +0000

    console page made prettier

--- consoles/views.py
+++ consoles/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from consoles.models import Console
@@ -13,3 +14,18 @@ def index(request):
 def get_console_by_id(request, id):
     context = {'console': get_object_or_404(Console, pk=id)}
     return render(request, 'consoles/console_details.html', context)
+
+
+def get_console_by_copies_sold(request):
+    consoles = Console.objects.all().order_by('-copies_sold')
+    return consoles
+
+
+def get_console_latest_releases(request):
+    consoles = Console.objects.all().order_by('-release_date')
+    return consoles
+
+
+def get_gta(request):
+    gta = Console.objects.filter(pk=8)
+    return gta
