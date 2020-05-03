6649c048704cbb410a45ca7a2c1bef15c9849e8c
Loki Hopkins <nyms.hopkins@gmail.com>
2020-05-03 17:21:35 +0000

    big progress with register/login/profile

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('register', views.register, name='register'),
     path('login', LoginView.as_view(template_name="users/login.html"), name="login"),
     path('logout', LogoutView.as_view(next_page="/"), name="logout"),
-    path('profile', views.profile_display, name="profile")
+    path('profile', views.profile, name="profile"),
+    path('profile/edit', views.update_profile, name="update_profile")
 ]
