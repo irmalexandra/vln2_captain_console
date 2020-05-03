dddca528688bca791b9c206eeef9de295a03e17f
R1klus <rikhardur.fridgeirsson@gmail.com>
2020-05-03 19:41:37 +0000

    added filter and sort

--- games/urls.py
+++ games/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     # localhost:8000/games/  <--- Path so far
-    path('', views.index, name="games-index"),
+    path('', views.index, name="index"),
     path('<int:id>', views.get_game_by_id, name="game_details"),
     path('test', views.get_game_by_copies_sold),
-    path('price', views.sort_by_price)
+    path('price', views.sort_by_price, name="price")
 ]
