5ea3b3565b5f23ad33afd506035b408ab4c4bffb
The69Kang <Emmsje legend>
2020-05-03 18:58:52 +0000

    sorter filter guy thing

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     # localhost:8000/   <---- Path so far
     path('', views.index, name="main-index"),
-    path('search/', views.search, name="search-results")
+    path('search/', views.search, name="search-results"),
+
 ]
