37b8f70ed64f488a7a63ebf47807cd6696a358dc
The69Kang <Emmsje legend>
2020-05-03 11:55:06 +0000

    random changes, mainly admin fixes

--- games/views.py
+++ games/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from games.models import Game
@@ -13,3 +14,8 @@ def index(request):
 def get_game_by_id(request, id):
     context = {'game': get_object_or_404(Game, pk=id)}
     return render(request, 'games/game_details.html', context)
+
+
+def get_game_by_copies_sold(request):
+    games = {'top_sellers': Game.objects.all().order_by('-copies_sold')}
+    return games
--- main/views.py
+++ main/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
+from games import views
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    top_sellers = views.get_game_by_copies_sold(request)
+    for thing in top_sellers['top_sellers']:
+        print(thing)
+    return render(request, 'main/index.html', top_sellers)
