142db816d7e6ad5bee65ffa240a55c73eb165151
mg <margret@margud.com>
2020-05-04 10:06:44 +0000

    filter and sort

--- consoles/views.py
+++ consoles/views.py
@@ -11,19 +11,16 @@ def index(request):
     return render(request, 'consoles/index.html', context)
 
 
+def sort_by_price(request):
+    context = {'consoles': Console.objects.all().order_by('price'),
+               "consoles_tab": "active"}
+    return render(request, 'consoles/index.html', context)
+
+
 def get_console_by_id(request, id):
     context = {'console': get_object_or_404(Console, pk=id)}
     return render(request, 'consoles/console_details.html', context)
 
-
-
-def get_console_offers(request):
-    all = Console.objects.all()
-    offers = []
-    for console in all:
-        if console.onSale == True:
-            offers.append(console)
-    return offers
 
 def get_console_by_copies_sold(request):
     consoles = Console.objects.all().order_by('-copies_sold')
@@ -33,4 +30,10 @@ def get_console_by_copies_sold(request):
 def get_console_latest_releases(request):
     consoles = Console.objects.all().order_by('-release_date')
     return consoles
+
+
+def get_console_offers(request):
+    return Console.objects.filter(onSale=True)
+
+
 
