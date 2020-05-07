c321dfcf42d651aef6f1c0b44f4996ab59760a65
Loki Hopkins <nyms.hopkins@gmail.com>
2020-05-07 19:44:26 +0000

    created forms and views for order account info payment/shipping

--- users/views.py
+++ users/views.py
@@ -2,8 +2,11 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 from users.models import Profile
-from users.forms.update_profile_form import EditProfileForm, EditUserForm
+from carts.models import ShippingInformation, PaymentInformation
+from users.forms.update_profile_form import ProfileForm, UserForm
 from users.forms.register_form import RegisterForm
+from carts.forms.payment_form import PaymentForm
+from carts.forms.shipping_form import ShippingForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
@@ -74,8 +77,8 @@ def update_profile(request):
     current_profile = Profile.objects.filter(user=request.user).first()
 
     if request.method == 'POST':
-        profile_form = EditProfileForm(instance=current_profile, data=request.POST)
-        user_form = EditUserForm(instance=current_profile.user, data=request.POST)
+        profile_form = ProfileForm(instance=current_profile, data=request.POST)
+        user_form = UserForm(instance=current_profile.user, data=request.POST)
 
         if profile_form.is_valid() and user_form.is_valid():
             profile_form.save()
@@ -83,16 +86,47 @@ def update_profile(request):
             return redirect('profile')
 
     return render(request, 'users/update_profile.html', {
-        'profile_form': EditProfileForm(instance=current_profile),
-        'user_form': EditUserForm(instance=current_profile.user)
+        'profile_form': ProfileForm(instance=current_profile),
+        'user_form': UserForm(instance=current_profile.user)
     })
 
 
 @login_required
 def update_payment_info(request):
-    pass
+    current_profile = Profile.objects.filter(user=request.user).first()
+    current_payment_info = PaymentInformation.objects.filter(id=current_profile.payment_information_id.id).first()
+    payment_form = PaymentForm(instance=current_payment_info)
 
+    if current_payment_info == None:
+        current_payment_info = PaymentInformation()
+        current_payment_info.save()
+
+    if request.method == 'POST':
+        payment_form = PaymentForm(instance=current_payment_info, data=request.POST)
+
+        if payment_form.is_valid():
+            payment_form.save()
+            return redirect('profile')
+    print()
+    return render(request, 'users/update_payment_info.html', {
+        'payment_form': payment_form
+    })
 
 @login_required
 def update_shipping_info(request):
-    pass
+    current_profile = Profile.objects.filter(user=request.user).first()
+    current_shipping_info = ShippingInformation.objects.filter(id=current_profile.shipping_information_id.id).first()
+    if current_shipping_info == None:
+        current_shipping_info = ShippingInformation()
+        current_shipping_info.save()
+
+    if request.method == 'POST':
+        current_shipping_form = ShippingForm(instance=current_shipping_info, data=request.POST)
+
+        if current_shipping_form.is_valid():
+            current_shipping_info.save()
+            return redirect('profile')
+
+    return render(request, 'users/update_shipping_info.html', {
+        'shipping_form': ShippingForm(instance=current_shipping_info)
+    })
